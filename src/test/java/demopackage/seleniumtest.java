df88a8871a4c9a5dfebcc486bab0685e95bf7a4d
amit.luthra1992@gmail.com <amit.luthra1992@gmail.com>
2024-06-06 22:05:10 +0530

    first project

---
+++ src/test/java/demopackage/seleniumtest.java
@@ -0,0 +1,51 @@
+package demopackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class seleniumtest {
+	 
+	public static WebDriver driver;
+	public String baseUrl;
+	
+	@BeforeClass
+	public void initbrowser()
+	{
+		 System.setProperty("webdriver.chrome.driver", "C://Users//Amit Luthra//demo05//sanjay//lib//chromedriver.exe");
+		 String environment = System.getProperty("env", "dev");
+	        switch (environment.toLowerCase()) {
+	            case "qa":
+	                baseUrl = "https://demo.automationtesting.in/Register.html";
+	                break;
+	            case "dev":
+	            default:
+	                baseUrl = "https://demo.automationtesting.in/Register.html";
+	                break;
+	        }
+	
+	}
+	
+	@Test
+	public void test1() throws InterruptedException
+	{
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Hello Sanjay");
+		
+		//driver.findElement(By.className("pass")).sendKeys("Hello Sanjay");
+		Thread.sleep(6000);
+		
+	}
+	
+	@AfterClass
+	public void quit()
+	{
+		driver.quit();
+	}
+	
+
+}
